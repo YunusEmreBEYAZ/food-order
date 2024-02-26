554c0b312687df8c969c2f92507035905e4f7ee3
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2024-02-26 12:39:29 +0100

    working styleing hover

--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function MenuItem() {
     return (
-<div className="bg-gray-800 p-4 rounded-lg text-center">
-                <Image className='rounded-lg' src="/pizza1.jpg" alt="Pizza" width={300} height={200} />
+<div className="bg-gray-800 p-4 rounded-lg text-center hover:bg-gray-600 hover:shadow-2xl hover:shadow-black transition-all cursor-pointer">
+                <Image className='rounded-lg mx-auto' src="/pizza1.jpg" alt="Pizza" width={300} height={200} />
                 <h3 className="uppercase font-semibold text-red-500 my-4"> Vegan Pizza</h3>
                 <p className="text-gray-100 text-sm">
                     lorem ipsum dolor sit amet, consectetur adipiscing elit.

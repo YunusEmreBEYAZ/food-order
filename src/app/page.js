7ae948186903ec655676b933e3cb34eddb0b2ac2
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2024-03-05 15:29:07 +0100

    added mongodb

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 
-import Header from "@/components/layout/Header";
-import Hero from "@/components/layout/Hero";
-import HomeMenu from "@/components/layout/HomeMenu";
-import SectionHeaders from "@/components/layout/SectionHeaders";
+import Hero from "../components/layout/Hero";
+import HomeMenu from "../components/layout/HomeMenu";
+import SectionHeaders from "../components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
-
     <Hero />
     <HomeMenu />
     <section className="text-center my-16">

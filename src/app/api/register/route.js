9134e493669c3a74115e8124d7042e384e7bf16d
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2024-03-18 00:25:24 +0100

    comment lines deleted

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -12,26 +12,5 @@ export async function POST(req) {
 
     const createdUser = await User.create(body);
     return Response.json(createdUser);
-    // try {
-    //     await mongoose.connect(process.env.MONGO_URL, {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //     });
 
-
-    //     console.log('Succesfully connected to MongoDB');
-    //     console.log('Request Body:', req.body);
-    //     const email = req.body.email;
-    //     const password = req.body.password;
-    //     const createdUser = await UserModel.create({email,password});
-    //     console.log('Received email:', email);
-    //     console.log('Received password:', password);    
-    //     console.log('User created:', createdUser);
-
-    //     return res.status(201).json(createdUser);
-
-    // } catch (error) {
-    //     console.error(error, error.message);
-    //     return;
-    // }
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -20,28 +20,6 @@ export default function Register() {
                      }
                     });
 
-        // try {
-        //     const response = await fetch('/api/register', {
-        //         method: 'POST',
-        //         body: JSON.stringify({email,password}),
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         }
-
-                
-        //     });
-
-        //     if(!response.ok) {
-        //         throw new Error(response.statusText);
-        //     }
-            
-
-        //     console.log(email, password);
-        //     console.log("User created successfully");
-        // } catch (error) {
-        //     console.error('Registration failed:', error.message);
-        // }
-
     }
         
     return (

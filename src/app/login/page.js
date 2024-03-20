125de2972088459116248d16b0566b1fad681bf7
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2024-03-20 14:07:46 +0100

    login gape design completed

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,8 @@
 "use client"
 
 import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Login() {
 
@@ -10,7 +12,7 @@ export default function Login() {
     return (
         <section className=" max-w-lg rounded-lg text-center bg-gray-600 shadow-2xl shadow-black transition-all mx-auto mt-16 mb-16 px-8 py-8">
         <h1 className="text-center text-white text-4xl">Login</h1>
-        <form>
+        <form className="block max-w-xs text-center mx-auto mt-16">
             <input
                 
                 type="email"
@@ -25,6 +27,17 @@ export default function Login() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
+            <button  type="submit">Login</button>
+            <div className="my-4 text-white italic">
+                    or login with...
+            </div>
+            <button className="bg-gray-100 hover:bg-blue-300 text-gray-900 flex gap-4 justify-center">
+                <Image src="/google.png" alt="Google Logo" width={20} height={20} />
+                Login with Google
+            </button>
+            <div className='text-center text-white mt-6 italic'>
+                    Don't have an account? <Link href={"/register"} className='underline'> Register here</Link>
+            </div>
         </form>
     </section>
     )
